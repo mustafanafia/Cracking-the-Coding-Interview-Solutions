9af0788e9226426de6816a89d408b9c7be48baba
Mustafa Nafia <55895079+mustafanafia@users.noreply.github.com>
2025-04-25 13:52:32 -0400

    Add files via upload

---
+++ String Rotation/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isRotation("waterbottle", "erbottlewat"));
+
+    }
+
+    static boolean isRotation(String s1, String s2){
+        int len = s1.length();
+
+        if(len == s2.length() && len > 0){
+            String s1s1 = s1 + s1;
+            return isSubstring(s1s1, s2);
+        }
+        return false;
+    }
+    static private boolean isSubstring(String s1, String s2) {
+        return s1.contains(s2);
+    }
+}
